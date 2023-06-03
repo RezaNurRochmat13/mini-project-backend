282ae856b9f20d2af912cd909d05cee9affda562
Reja <rezanurrochmat@gmail.com>
2023-06-03 18:23:38 +0700

    todo backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const PORT = process.env.PORT || 8081
+const app = express();
+const todoController = require('./controller/todo.controller')
+
+
+app.use(express.json())
+app.use(todoController)
+
+app.get('/', (req, res) => {
+  return res.send(200)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
