a4cdc8711050b8f9dc10074ff74ccfa2ea145b64
Reja <rezanurrochmat@gmail.com>
2023-06-03 19:00:45 +0700

    blogs api

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const PORT = process.env.PORT || 8081
 const app = express();
 const todoController = require('./controller/todo.controller')
+const blogController = require('./controller/blog.controller')
 
 
 app.use(express.json())
 app.use(todoController)
+app.use(blogController)
 
 app.get('/', (req, res) => {
   return res.send(200)
